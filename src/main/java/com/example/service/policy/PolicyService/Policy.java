0597a91801f5e83b68e14d493ee51269f876ee59
Richard Schilhavy <richard.schilhavy@gmail.com>
2018-11-17 00:18:07 -0500

    Policy entity, JSON object marshalling, simple GET policy object using rest

--- src/main/java/com/example/service/policy/PolicyService/Policy.java
+++ src/main/java/com/example/service/policy/PolicyService/Policy.java
@@ -1,6 +1,55 @@
 package com.example.service.policy.PolicyService;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.ws.rs.ConstrainedTo;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Currency;
+import java.util.Locale;
+
+@Entity
+@Table(name = "policy")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Policy {
 
     // attributes
+    @Id
+    private Long policyNo;
+    private String firstName;
+    private String lastName;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private BigDecimal deductible;
+    private final Currency usd = Currency.getInstance(Locale.US);
+
+    // explicit constructor
+    public Policy (Long policyNo, String firstName, String lastName, LocalDate startDate, LocalDate endDate, BigDecimal deductible)
+    {
+        this.policyNo = policyNo;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.deductible = deductible;
+    }
+
+    // explicit constructor with Strings for complex datatypes
+    public Policy (Long policyNo, String firstName, String lastName, String startDate, String endDate, String deductible)
+    {
+        this(policyNo, firstName, lastName, LocalDate.parse(startDate), LocalDate.parse(endDate), new BigDecimal(deductible));
+    }
+
+    @Override public String toString() {
+        return "I'm a Policy";
+    }
 }
--- src/main/java/com/example/service/policy/PolicyService/PolicyServiceController.java
+++ src/main/java/com/example/service/policy/PolicyService/PolicyServiceController.java
@@ -3,11 +3,35 @@ package com.example.service.policy.PolicyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 @RestController
 public class PolicyServiceController {
 
-    @GetMapping("/policy")
+    // TODO Create One/Many Policy (POST)
+
+    // TODO Read One/Many Policy (GET)
+
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello World";
     }
+
+    @GetMapping("/policy")
+    public Policy getPolicy() {
+        Policy p = new Policy (
+                1233456L,
+                "John",
+                "Smith",
+                LocalDate.of(2018,11,16),
+                LocalDate.of(2019,11,16),
+                BigDecimal.valueOf(125.5)
+                );
+        return p;
+    }
+
+    // TODO Update One Policy (PUT)
+
+    // TODO Delete One Policy (DELETE)
 }
