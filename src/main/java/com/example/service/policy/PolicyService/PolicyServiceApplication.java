a3ef96f49057233a58af79ecaf1d036be023d37c
Richard Schilhavy <richard.schilhavy@gmail.com>
2018-11-16 21:06:24 -0500

    Requirements and tasks v1

--- src/main/java/com/example/service/policy/PolicyService/PolicyServiceApplication.java
+++ src/main/java/com/example/service/policy/PolicyService/PolicyServiceApplication.java
@@ -2,9 +2,10 @@ package com.example.service.policy.PolicyService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PolicyServiceApplication {
+public class PolicyServiceApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolicyServiceApplication.class, args);
--- src/main/java/com/example/service/policy/PolicyService/PolicyServiceController.java
+++ src/main/java/com/example/service/policy/PolicyService/PolicyServiceController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PolicyServiceController {
 
     @GetMapping("/policy")
-    Policy newPolicy() {
-        return new Policy();
+    public String sayHello() {
+        return "Hello World";
     }
 }
