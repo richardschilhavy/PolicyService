2b39bcb6deff4aef17992a90d9fbbaa1f6d9be19
Richard Schilhavy <richard.schilhavy@gmail.com>
2018-11-16 21:37:14 -0500

    Requirements and tasks v1

--- src/main/java/com/example/service/policy/PolicyService/PolicyServiceApplication.java
+++ src/main/java/com/example/service/policy/PolicyService/PolicyServiceApplication.java
@@ -1,17 +1,22 @@
 package com.example.service.policy.PolicyService;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
+
 @SpringBootApplication
 public class PolicyServiceApplication extends SpringBootServletInitializer {
+
+	/*public static void main(String[] args) {
+		SpringApplication.run(PolicyServiceApplication.class, args);
+	}*/
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(PolicyServiceApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolicyServiceApplication.class, args);
